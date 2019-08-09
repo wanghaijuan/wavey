4b45af6943cf283bccf59cb53c4f14a81d4d1b73
wanghaijuan <haijuanHN@yahoo.com>
2019-08-09 16:41:56 +0800

    Update index.javascript.js

--- js/index.javascript.js
+++ js/index.javascript.js
@@ -1,25 +1,24 @@
 
-window.onload = function() {
-  var waveOne = wavify(document.querySelector('#wave-one'), {
-    height: 80,
-    bones: 4,
-    amplitude: 60,
-    color: "#F1F1F1",
-    speed: .15
-  });
-  var waveTwo = wavify( document.querySelector('#wave-two'), {
-    height: 60,
-    bones: 3,
-    amplitude: 40,
-    color: "rgba(241, 241, 241, .8)",
-    speed: .25
-  });
-  var waveThree = wavify( document.querySelector('#wave-three'), {
-    height: 40,
-    bones: 5,
-    amplitude: 20,
-    color: "rgba(241, 241, 241, .6)",
-    speed: .2
-  });
-}
+
+var waveOne = wavify(document.querySelector('#wave-one'), {
+  height: 80,
+  bones: 4,
+  amplitude: 60,
+  color: "#F1F1F1",
+  speed: .15
+});
+var waveTwo = wavify( document.querySelector('#wave-two'), {
+  height: 60,
+  bones: 3,
+  amplitude: 40,
+  color: "rgba(241, 241, 241, .8)",
+  speed: .25
+});
+var waveThree = wavify( document.querySelector('#wave-three'), {
+  height: 40,
+  bones: 5,
+  amplitude: 20,
+  color: "rgba(241, 241, 241, .6)",
+  speed: .2
+});
 
